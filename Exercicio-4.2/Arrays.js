36f83810b7dfe9b54b1f88f68d42a57041267b76
George Santos <george.santos_ufmg@outlook.com>
2022-05-16 16:08:20 -0300

    Exercicios do 1 ao 9

---
+++ Exercicio-4.2/Arrays.js
@@ -0,0 +1,88 @@
+// Exercício 1
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for(index = 0; index < numbers.length; index += 1){
+//   console.log(numbers[index]);
+// }
+
+// Exercício 2
+// let result = 0;
+
+// for(index = 0; index < numbers.length; index += 1){  
+//   result += numbers[index];
+// }
+
+// console.log(result);
+
+// Exercício 3
+
+// let media = result/numbers.length 
+
+// console.log(media);
+
+// Exercício 4 
+
+// if (media > 20){
+//   console.log('valor maior que 20');
+// } else{
+//   console.log('valor menor ou igual a 20');
+// }
+
+// Exercício 5 
+
+// let maior = 0
+
+// for(index = 0; index < numbers.length; index += 1){
+//   if (numbers[index] > maior){
+//     maior = numbers[index]
+//   } else{
+//     maior = maior
+//   }
+// }
+// console.log(maior);
+
+// Exercício 6 
+// let quantidadeDeNumerosImpares = 0;
+
+// for(index = 0; index < numbers.length; index +=1){
+//   if(numbers[index]%2 !== 0){
+//     quantidadeDeNumerosImpares +=1;
+//   }
+// }
+
+// if(quantidadeDeNumerosImpares === 0){
+//   console.log('Nenhum valor impar encontrado');
+// } else{
+//   console.log(quantidadeDeNumerosImpares);
+// }
+
+// Exercício 7
+// let menor = numbers[0];
+
+// for (index =0; index < numbers.length; index +=1){
+//   if (numbers[index] < menor) {
+//     menor = numbers[index];
+//   }
+// }
+
+// console.log(menor);
+
+// Exercício 8
+
+// let numbers = [];
+
+// for( index = 1; index <= 25; index += 1){
+//   numbers.push (index)
+// }
+
+// console.log(numbers);
+
+// Exercício 9
+
+// let metadeNumbers = [];
+
+// for( index = 0; index < numbers.length; index +=1){
+//   metadeNumbers.push (numbers[index]/2)
+// }
+// console.log(metadeNumbers);
