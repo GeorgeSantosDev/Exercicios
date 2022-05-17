4731d0a2c7de303ab8b7e3a2d643fb4713831c04
George Santos <george.santos_ufmg@outlook.com>
2022-05-16 21:44:44 -0300

    EXercicio 1

---
+++ Exercicio-4.3/logica.js
@@ -0,0 +1,29 @@
+// Exercicio 1 
+// let fatorial10 = 1;
+
+// for (let index = 1; index < 11; index +=1 ) {
+//   fatorial10 = fatorial10 * [index];
+// }
+
+// console.log(fatorial10); 
+
+// Exercicio 2 
+
+// let word = 'tryber';
+// let reverseWord = '';
+
+// for (let index = word.length; index < 0; index -=1 ){
+//  reverseWord += word[index];
+// }
+
+// console.log(reverseWord);
+
+// Exercicio 3
+
+// Exercicio 4
+
+let maiorPrimo = 0;
+
+for(index = 0; index < 51; index += 1){
+  if([index]%2 )
+}
