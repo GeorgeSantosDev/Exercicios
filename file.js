e1550f944dcb442ac35cc56bb6eda70c2888a47d
George Santos <george.santos_ufmg@outlook.com>
2022-05-20 19:02:09 -0300

    inciando exercicios

---
+++ file.js
@@ -0,0 +1,21 @@
+// let NewElement = document.createElement('Section');
+
+// document.querySelector("#pai").appendChild(NewElement);
+
+// let NewElementSon = document.createElement('p'); 
+
+// document.querySelector("#elementoOndeVoceEsta").appendChild(NewElementSon);
+
+// let NewSon = document.createElement('title');
+
+// document.querySelector('#primeiroFilhoDoFilho').appendChild(NewSon);
+
+let a = document.querySelector('#pai').children;
+
+for (index = 0; index < a.length; index += 1){
+
+  if(a[index] !== document.querySelector('#elementoOndeVoceEsta')){
+    document.querySelector('#pai').removeChild(a[index]);
+  }
+
+}
